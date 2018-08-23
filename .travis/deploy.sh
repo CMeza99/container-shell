c60be867363f27242d1b470e06ece7f18278f245
Carlos Meza <carlos@digitalr00ts.com>
2018-08-23 02:48:15 -0700

    troubleshooting travis deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# test -z "$1" && exit 1
-# test -z "$2" && exit 1
-
-
 [ "${TRAVIS_PULL_REQUEST}" != 'false' ] && exit
 [ "${TRAVIS_BRANCH}" != 'develop' ] && exit
 
@@ -12,10 +8,7 @@ GIT_COMMITTER_NAME='Travis CI'
 GIT_REPO_PUSH="git@$(git remote -v | head -n1 | grep -o --color=never 'github\.com.* ' | sed 's/\//\:/')"
 GIT_BASE='master'
 
-# mkdir -p ~/.ssh/
-# openssl aes-256-cbc -K $1 -iv $2 -in .travis/id_ed25519.enc -out ~/.ssh/id_ed25519 -d
-# chmod 700 ~/.ssh/
-# chmod 400 ~/.ssh/id_ed25519
+ls -hAl ~/.ssh/
 
 git fetch origin ${GIT_BASE}:${GIT_BASE}
 git checkout ${GIT_BASE}
